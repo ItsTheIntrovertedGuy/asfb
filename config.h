69164534b461edfffec15f8d1e967d76c755d4f3
ItsTheIntrovertedGuy <max0018@t-online.de>
2021-07-12 12:24:33 +0200

    Updated config

--- config.h
+++ config.h
@@ -41,4 +41,5 @@ global_variable file_type_config GLOBALFileTypeConfig[] = {
 	{ ".mp3",       "/bin/mpv",                0            },
 	{ ".flac",      "/bin/mpv",                0            },
 	{ ".ogg",       "/bin/mpv",                0            },
+	{ ".wma",       "/bin/mpv",                0            },
 };
